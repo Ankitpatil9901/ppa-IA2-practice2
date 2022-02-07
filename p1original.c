bfa2c3bcb89393ae9051f7f54bfde26a21f9428d
Ankit Patil <96898434+Ankitpatil9901@users.noreply.github.com>
2022-02-07 09:06:59 +0000

    nothing

--- p1original.c
+++ p1original.c
@@ -1,2 +1,25 @@
+#include<stdio.h>
+void input(float *base,float *height)
+{
+  printf("Enter the value of height and Base:");
+  scanf("%f %f",base,height);
+}
 
+void find_area(float base,float height,float *area)
+{
+  *area=0.5*base*height;
+}
+
+void output(float base,float height,float area)
+{
+  printf("Area of triangle is:%f",area);
+}
+
+int main ()
+{
+  float x,y,z;
+  input(&x,&y);
+  find_area(x,y,&z);
+  output(x,y,z);
+  return 0;
 }
