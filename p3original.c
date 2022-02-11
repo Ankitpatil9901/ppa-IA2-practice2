c99d511837795867b69ff368574b5f9520468fac
Ankit Patil <96898434+Ankitpatil9901@users.noreply.github.com>
2022-02-11 04:19:56 +0000

    nothing

--- p3original.c
+++ p3original.c
@@ -1,1 +1,38 @@
 #include<stdio.h>
+int input()
+{
+  int n;
+  printf("Enter the number\n");
+  scanf("%d",&n);
+  return n;
+}
+
+int composite(int n)
+{
+  for(int i=1;i<=n/2;i++)
+   {
+    if (n/i!=1)
+    {
+      return 1;
+    }
+    return -1;
+   }
+}
+
+void output(int n,int c)
+{
+  if (c>0)
+  printf("%d is composite",n);
+else
+printf("%d is not composite",n);
+}
+int main()
+{
+  int n,c;
+  n=input();
+  c=composite(n);
+  output(n,c);
+  return 0;
+}
+
+
